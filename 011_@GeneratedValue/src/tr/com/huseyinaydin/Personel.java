0c7f2c373d3c61ef4fde07c02259d0cc15224c3c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-14 08:59:15 +0300

    Hibernate GeneratedValue dipnotu ile Id kolonuna otomatik artan değerler veriliyor.

---
+++ 011_@GeneratedValue/src/tr/com/huseyinaydin/Personel.java
@@ -0,0 +1,73 @@
+package tr.com.huseyinaydin;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+//بسم الله الرحمن الرحيم
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Hibernate.
+ *
+ */
+
+@Entity 
+@Table(name = "CALISANLAR") //TABLO ADI
+public class Personel implements Serializable {
+	
+	@Id
+	@GeneratedValue //BU KULLANIMDA ID ALANINA OTOMATIK ID DEGERLER VERILIYOR
+	private int personelId;
+	private String personelAdi;
+	private String personelSoyadi;
+	
+	@Temporal (TemporalType.DATE)
+	private Date personelKayitTarihi;
+	
+	@Lob
+	private String personelAdresi;
+	
+	private static final long serialVersionUID = 1L;
+	
+	public int getPersonelId() {
+		return personelId;
+	}
+	public void setPersonelId(int personelId) {
+		this.personelId = personelId;
+	}
+	public String getPersonelAdi() {
+		return personelAdi;
+	}
+	public void setPersonelAdi(String personelAdi) {
+		this.personelAdi = personelAdi;
+	}
+	public String getPersonelSoyadi() {
+		return personelSoyadi;
+	}
+	public void setPersonelSoyadi(String personelSoyadi) {
+		this.personelSoyadi = personelSoyadi;
+	}
+	public String getPersonelAdresi() {
+		return personelAdresi;
+	}
+	public void setPersonelAdresi(String personelAdresi) {
+		this.personelAdresi = personelAdresi;
+	}
+	public Date getPersonelKayitTarihi() {
+		return personelKayitTarihi;
+	}
+	public void setPersonelKayitTarihi(Date personelKayitTarihi) {
+		this.personelKayitTarihi = personelKayitTarihi;
+	}
+}
